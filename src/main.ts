84fdaa321a50e581301e91b07ea6cb3368dcdf98
diebitsh <slavaprolubnikov@mail.ru>
2023-04-01 14:23:47 +0300

    HtmlGeneratorService

--- src/main.ts
+++ src/main.ts
@@ -1,105 +1,72 @@
+import { Command } from 'commander';
 import { trace } from 'console';
 import * as fs from 'fs';
+import { readFileSync } from 'fs';
+import { ComparableDocument } from './models/comparable-document.model';
+import { Line } from './models/line.model';
+import { ViewableLine } from './models/viewable-line.model';
+import { ConsolePrinter } from './services/console-printer.service';
+import { Differ } from './services/differ';
 
-class Line {
 
-    constructor(line: string, lineIndex: number) {
-        this.LineValue = line;
-        this.LineIndex = lineIndex;
-    }
+const program = new Command();
+program
+    .version("1.0")
+    .description("CLI приложения для сравнения двух html")
+    .option("--compare <paths...>", "Сравнение файлов")
+    .parse(process.argv);
+    
+const options = program.opts();
 
-    LineValue: string;
-    LineIndex: number;
+if (options == null) {
+    program.help();
 }
-class ComparableDocument {
 
-    constructor (lines: Line[] ) {
-        this.Lines = lines;
+const source: ComparableDocument = new ComparableDocument(
+    loadFile("C://old.html").toString().split('\n').map( (line, index) => new Line(line.replace('\r', ''), index+1) )
+)
+
+const dest: ComparableDocument = new ComparableDocument(
+    loadFile("C://new.html").toString().split('\n').map( (line, index) => new Line(line.replace('\r', ''), index+1) )
+)
+
+const test = new Differ(source, dest);
+const printer = new ConsolePrinter();
+
+printer.print(test.diff());
+var lines = test.diff();
+createResultHtml(test.createHtmlView(lines));
+if (options.compare) {
+
+    const paths: string[] = options.compare as string[];
+    if (!paths || paths.length < 2) {
+       throw new Error(`Не указан путь(и) до файлов ${paths}`);
     }
 
-    Lines: Line[];
+    // const source: ComparableDocument = new ComparableDocument(
+    //     loadFile(paths[0]).toString().split('\n').map( (line, index) => new Line(line.replace('\r', ''), index+1) )
+    // )
+    
+    // const dest: ComparableDocument = new ComparableDocument(
+    //     loadFile(paths[1]).toString().split('\n').map( (line, index) => new Line(line.replace('\r', ''), index+1) )
+    // )
+    
+    // const test = new Differ(source, dest);
+    // const printer = new ConsolePrinter();
+
+    // printer.print(test.diff());
+    // var lines = test.diff();
+    // createResultHtml(test.createHtmlView(lines));
 }
 
-class ViewableLine {
-
-    constructor(editType: string, oldLine: Line | null, newLine: Line | null) {
-        this.EditType = editType;
-        this.OldLine = oldLine;
-        this.NewLine = newLine;
-    }
-
-    EditType: string;
-    OldLine: Line | null;
-    NewLine: Line | null;
-
-    public getOldLineNumber(): string {
-        if (this.OldLine?.LineIndex == null) {
-            return ""
-        }
-        return this.OldLine.LineIndex.toString();
-    }
-
-    public getNewLineNumber(): string {
-        if (this.NewLine?.LineIndex == null) {
-            return ""
-        }
-        return this.NewLine.LineIndex.toString();
-    }
-
-    public getlineText(): string {
-        if (this.NewLine == null && this.OldLine != null) {
-            return this.OldLine?.LineValue;
-        }
-        else if (this.OldLine == null && this.NewLine != null) {
-            return this.NewLine?.LineValue;
-        }
-        else if (this.OldLine?.LineValue == this.NewLine?.LineValue && this.OldLine?.LineValue != null) {
-            return this.OldLine?.LineValue;
-        }
+function loadFile(filePath: string): string {
+    try {
+        const file = readFileSync(filePath, "utf-8");
+        return file;
+    } catch (exception) {
+        console.error(exception);
         return "";
     }
-
-}
-
-class ConsolePrinter {
-
-    public print(diff: ViewableLine[]): void {
-        diff.reverse().forEach( x=> {
-            this.printEdit(x);
-        })
-    }
-
-    public printEdit(edit: ViewableLine) : void {
-        let tag;
-        let color;
-
-        if (edit.EditType == "inserted") {
-            tag = "+";
-        }
-        else if (edit.EditType == "deleted") {
-            tag = "-";
-        }
-        else {
-            tag = " "
-        }
-        if (edit.EditType == "inserted") {
-            color = "\u001b[32m";
-        }
-        else if (edit.EditType == "deleted") {
-            color = "\u001b[31m";
-        }
-        else {
-            color = " "
-        }
-        const reset = "\u001b[30m"
-
-        const oldLineNumber = edit.getOldLineNumber();
-        const newLineNumber = edit.getNewLineNumber();
-        const lineText = edit.getlineText();
-
-        console.log("\u001b[30m",color,tag, "\t", oldLineNumber, "\t" , newLineNumber, "\t", lineText);
-    }
-
 }
 
 class Differ {
@@ -245,17 +212,7 @@ class Differ {
 
 }
 
-// fs.readFile('./test-pages/1-dst.html', (err, data) => {
-//     if (err) throw err;
-//     console.log(data.toString());
-// });
 
-const source: ComparableDocument = new ComparableDocument(
-    fs.readFileSync('././test-pages/source.html', 'utf-8').toString().split('\n').map( (line, index) => new Line(line.replace('\r', ''), index+1) )
-)
-const dest: ComparableDocument = new ComparableDocument(
-    fs.readFileSync('././test-pages/dest.html', 'utf-8').toString().split('\n').map( (line, index) => new Line(line.replace('\r', ''), index+1) )
-)
 
 const test = new Differ(source, dest);
 const printer = new ConsolePrinter();
